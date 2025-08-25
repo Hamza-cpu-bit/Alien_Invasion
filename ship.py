7ae24398fa86b5b57f269aa09c304e5a1884a3bc
Hamza <m.hamzakhan177@gmail.com>
2025-08-25 01:27:37 -0700

    updated movement and added ship

---
+++ ship.py
@@ -0,0 +1,34 @@
+import pygame
+
+
+class Ship:
+    def __init__(self, ai_game):
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        self.screen_rect = self.screen.get_rect()
+
+        # Load the ship image
+        self.image = pygame.image.load('images/ship.png')
+        self.image = pygame.transform.scale(self.image, (60, 60))
+        self.rect = self.image.get_rect()
+
+        # Start each new ship at the bottom center
+        self.rect.midbottom = self.screen_rect.midbottom
+
+        # Store a float for the ship's exact horizontal position
+        self.x = float(self.rect.x)
+
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        # Update rect object from self.x
+        self.rect.x = self.x
+
+    def blitme(self):
+        self.screen.blit(self.image, self.rect)
